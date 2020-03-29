448af8188857b3b0c9b8bf67a9de8a07b1b1a1d9
Justin Kennedy <iamjustinkennedy@gmail.com>
2020-03-28 20:26:36 -0400

    tims a pussy

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const title = 'LINSKY DRINK';
+const title = 'TIMS A PUSSY';
 ReactDOM.render(
   <h1>{title}</h1>,
   document.getElementById('app')
